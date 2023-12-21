a71c73c59deac9516f9e54f555a33f7b17809096
HyperWinX <artmsmth@gmail.com>
2023-12-21 20:24:16 +0400

    Added array and loops guide

---
+++ cpp/arrays/main.c
@@ -0,0 +1,6 @@
+int main(){
+	int array[3] = {0}; //Creating array of integers for 3 elements and zeroing it out
+	array[0] = 1; //Setting elements
+	array[1] = 2;
+	array[2] = 3;
+}
---
+++ cpp/loops/main.c
@@ -0,0 +1,12 @@
+int main(){
+	int array[10] = {0}
+	//Loop through array and fill it using for loop
+	for (int i = 0; i < 10; i++){
+		array[i] = i;
+	}
+	//While loop example
+	int num = 0;
+	while (num < 10){
+		num++;
+	}
+}
