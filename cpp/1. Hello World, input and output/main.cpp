ce2f221ec04f6059f9feb40c1777651a72fd35bb
HyperWinX <artmsmth@gmail.com>
2023-12-22 08:38:33 +0400

    C++ finished

--- cpp/1. Hello World, input and output/main.cpp
+++ cpp/1. Hello World, input and output/main.cpp
@@ -7,5 +7,8 @@ int main(int argc, char *argv[]){
 	//Nuh uh, its not end
 	int number = 666;
 	cout << "Demonic number: " << number << endl;
+	string str;
+	//Reading something from console
+	cin >> str;
 	//Now thats all. Thats your Hello World!
 }
